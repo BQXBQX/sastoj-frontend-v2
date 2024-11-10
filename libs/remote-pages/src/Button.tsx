e2515cdeb9f830f0c65647a0b153e328db892f27
bqxbqx <boqingxin14@gmail.com>
2024-11-10 11:56:09 +0800

    feat(ui): use semi library

--- libs/remote-pages/src/Button.tsx
+++ libs/remote-pages/src/Button.tsx
@@ -1,3 +1,12 @@
+import { Radio } from '@douyinfe/semi-ui';
+
 export const Button = () => {
-  return <>Button</>;
+  return (
+    <>
+      Button{' '}
+      <Radio aria-label="单选示例" name="demo-radio">
+        Radio
+      </Radio>
+    </>
+  );
 };
